c519dc16064a32fb5655a176c63da6f0a90f1af3
Tushar Gaikawad <68044895+Tushargaikwad464@users.noreply.github.com>
2020-07-14 17:53:03 +0530

    Add files via upload
    Assignment 1 with js
    Assignment 2
    Assignment 3

---
+++ DAY2/assignment 1.js
@@ -0,0 +1,17 @@
+function myfunction() {
+    for (i = 0; i < 10; i++) {
+        console.count();
+    }
+}
+
+function myfunction1() {
+    console.error("You made a mistake");
+}
+
+function myfunction2 {
+    console.log("Hello world!");
+    console.group();
+    console.log("Hello again, this time inside a group!");
+    console.groupEnd();
+    console.log("and we are back.");
+}
